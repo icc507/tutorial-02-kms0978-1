cfb7a6fb9be51d5505d9b27cd290c65312ec8486
kms0978 <114182131+kms0978@users.noreply.github.com>
2024-08-27 09:38:27 -0400

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -9,5 +9,38 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
+#t = input()
+#print(t)
+def arbolTrinario(numero):
+  return [numero, [], [], []]
+
+def insertaEnArbolTrinario(arbol, numero):
+  if arbol == []:
+    arbol += arbolTrinario(numero)
+  elif numero < arbol[0]:
+    insertaEnArbolTrinario(arbol[1], numero)
+  elif numero == arbol[0]:
+    insertaEnArbolTrinario(arbol[2], numero)
+  else:
+    insertaEnArbolTrinario(arbol[3], numero)
+
+def estaEnArbolTrinario(arbol, numero):
+  if arbol == []:
+    return False
+  elif numero == arbol[0]:
+    return True
+  elif numero < arbol[0]:
+    return estaEnArbolTrinario(arbol[1], numero)
+  elif numero == arbol[0]:
+    return estaEnArbolTrinario(arbol[2], numero)
+  else:
+    return estaEnArbolTrinario(arbol[3], numero)
+#uso
 t = input()
-print(t)
+m = t.split()
+num = int(m[0])
+w = arbolTrinario(num)
+for i in range(1,len(m)):
+  n = int(m[i])
+  insertaEnArbolTrinario(w, n)
+print(w)
